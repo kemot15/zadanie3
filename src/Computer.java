5d390c80f7bf10812de37435dabc4ceab6e5f227
Tomek <kemot15@tlen.pl>
2019-07-08 19:58:28 +0200

    ver 1.2

--- src/Computer.java
+++ src/Computer.java
@@ -2,4 +2,10 @@ public class Computer {
     String name;
     int ram;
     int disc;
+
+    Computer (String n, int r, int d){
+        name = n;
+        ram = r;
+        disc = d;
+    }
 }
--- src/ComputerTest.java
+++ src/ComputerTest.java
@@ -1,15 +1,15 @@
 public class ComputerTest {
     public static void main(String[] args) {
-        Computer computer1 = new Computer();
-        Computer computer2 = new Computer();
+        Computer computer1 = new Computer("HP", 8, 120);
+        Computer computer2 = new Computer( "Dell", 4, 240);
 
-        computer1.name = "HP";
-        computer1.ram = 8;
-        computer1.disc = 120;
-
-        computer2.name = "Dell";
-        computer2.ram = 4;
-        computer2.disc = 240;
+//        computer1.name = "HP";
+//        computer1.ram = 8;
+//        computer1.disc = 120;
+//
+//        computer2.name = "Dell";
+//        computer2.ram = 4;
+//        computer2.disc = 240;
 
         System.out.println("Komputer 1 - nazwa: " + computer1. name + " RAM: " + computer1.ram + " dysk: " + computer1.disc);
         System.out.println("Komputer 2 - nazwa: " + computer2. name + " RAM: " + computer2.ram + " dysk: " + computer2.disc);
